a0c988c3a6979b30aaa8ed581bd116895153ccc0
Tausif Khan <khantausifkhan123@gmail.com>
2023-06-25 19:00:18 +0530

    lode

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ let voices = [];
 
 let voiceSelect = document.querySelector("select");
 
+window.addEventListener("load", (event) => {
+    loadVoices()
+  });
+
 window.speechSynthesis.onvoiceschanged = () => {
   voices = window.speechSynthesis.getVoices();
   speech.voice = voices[0];
@@ -25,3 +29,15 @@ document.querySelector("button").addEventListener("click", () => {
   speech.text = document.querySelector("textarea").value;
   window.speechSynthesis.speak(speech);
 });
+
+
+function loadVoices() {
+    const synth = window.speechSynthesis
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      option.value = i;
+      voiceSelect.appendChild(option);
+    }
+}  
