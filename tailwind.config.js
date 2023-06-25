9c1fa5d9f575b06e256c237a6c7db5b410a54a5d
Tausif Khan <khantausifkhan123@gmail.com>
2023-06-25 13:39:56 +0530

    build

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+        
+      }
+    },
   },
   plugins: [],
 }
